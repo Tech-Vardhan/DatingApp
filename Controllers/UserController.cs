19f0b2fa41692fd5283f8a805eacd41db2c162df
Harsh <harsh.gurjar@techextensor.com>
2023-10-15 09:43:00 +0530

    User Controllers

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -7,24 +8,24 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class UserController : ControllerBase
 {
-    private readonly DataContext _context;
+     private readonly DataContext _context;
 
-    public UserController(DataContext context)
+     public UserController(DataContext context)
      {
-        _context = context;
-    }
-    
-    [HttpGet]
-    public ActionResult<IEnumerable<AppUser>>GetUsers()
-    {
-      var users = _context.Users.ToList();
+          _context = context;
+     }
 
-      return users;
-    }
+     [HttpGet]
+     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+     {
+          var users = await _context.Users.ToListAsync();
 
-    [HttpGet("{id}")]
-    public  ActionResult<AppUser>GetUser(int id)
-    {
-      return  _context.Users.Find(id);
-    }
+          return users;
+     }
+
+     [HttpGet("{id}")]
+     public async Task<ActionResult<AppUser>> GetUser(int id)
+     {
+          return await _context.Users.FindAsync(id);
+     }
 }
