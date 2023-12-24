3fcb10bbf5a3155baa392629c71c3f9e96cfed47
Jaimin Shah <jaimin.shah@techextensor.com>
2023-12-24 16:09:10 +0530

    separate logic from program.cs to new class

--- Program.cs
+++ Program.cs
@@ -1,18 +1,13 @@
-using API;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext> (opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-var app = builder.Build();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddCors();
-
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
